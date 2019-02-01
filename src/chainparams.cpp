bf150c41a756ef109d1186e85aa6371e5ffb9d41
Mircea Rila <mircea.rila@gmail.com>
2019-02-01 18:19:07 +0200

    update checkpoints

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -138,10 +138,10 @@ public:
         consensus.nGovernanceMinQuorum = 10;
         consensus.nGovernanceFilterElements = 20000;
         consensus.nMasternodeMinimumConfirmations = 15;
-        consensus.BIP34Height = 227931; // FIX
-        consensus.BIP34Hash = uint256S("0x000000000000024b89b42a942fe0d9fea3bb44ab7bd1b19115dd6a759c0808b8"); // FIX
-        consensus.BIP65Height = 619382; // 00000000000076d8fcea02ec0963de4abfd01e771fec0863f960c2c64fe6f357
-        consensus.BIP66Height = 245817; // 00000000000b1fa2dfa312863570e13fae9ca7b5566cb27e55422620b469aefa
+        consensus.BIP34Height = 150000;
+        consensus.BIP34Hash = uint256S("0x0000000005f483c970c60293a8d0508cfe6a4500359d26f84abdf302328f413d");
+        consensus.BIP65Height = 150000; // 00000000000b1fa2dfa312863570e13fae9ca7b5566cb27e55422620b469aefa
+        consensus.BIP66Height = 150000; // 00000000000b1fa2dfa312863570e13fae9ca7b5566cb27e55422620b469aefa
         consensus.DIP0001Height = 119000;
         consensus.powLimit = uint256S("00000fffff000000000000000000000000000000000000000000000000000000");
 
@@ -177,10 +177,10 @@ public:
         consensus.vDeployments[Consensus::DEPLOYMENT_BIP147].nThreshold = 3226; // 80% of 4032
 
         // The best chain should have at least this much work.
-        consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000000000dde3dfa3ad67fd");//117400
+        consensus.nMinimumChainWork = uint256S("0x00000000000000000000000000000000000000000000000000eee93cb0b1f4be");//269664
 
         // By default assume that the signatures in ancestors of this block are valid.
-        consensus.defaultAssumeValid = uint256S("0x000000000596de322263352224b2b9f5030fe63e003433791ecec5676755923b"); //117400
+        consensus.defaultAssumeValid = uint256S("0x000000004516f472b86c0fe081cbd4a0533b87b63ace4a9774d41a6cb53df37a"); //269664
 
         //Sparks stuff
         consensus.nSPKHeight = 100000;
@@ -249,37 +249,17 @@ public:
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
             (      0, uint256S("0x00000a5c6ddfaac5097218560d5b92d416931cfeba1abf10c81d1d6a232fc8ea"))
-	        (	 500, uint256S("0x00000004895911edc563a815f75563032bd17ef5322d0e37d58bff7f84c17274"))
-	        (	1000, uint256S("0x00000000020ff055aa61a4d11d1d66730d7e0534dc8570dc2d40c45b675b2582"))
-	        (   5000, uint256S("0x0000000000463bee3178a7539e04f4ec9d1df1608d8e2e8f4bc759c6cb866abb"))
-	        (  10000, uint256S("0x00000000000d9619d89073835b04dcfcb5f5188a0454daace5d9498953b277be"))
-	        (  15000, uint256S("0x0000000000336fe7dd80c9a785df28d2b5154551f5345539d62917cf9eb46c79"))
-	        (  20000, uint256S("0x00000000002d4f69f6e21101069e9ff65ca716380b2421147a505517d18d9bb4"))
-	        (  25000, uint256S("0x00000000009d92c292b41b83a213eeed3efa78c1e5b3fd205bec2d5d1483158f"))
-	        (  30000, uint256S("0x000000000005fbe7c003a4526e23ba4fe285f24e4f0b9931d75002871c3d87390"))
-	        (  35000, uint256S("0x00000000013480d93276ba86c9c5b7f7437fdf9ac433fc5bf52d0f59a3eea416"))
-	        (  40000, uint256S("0x00000000027275c13c78980d495b6f943bf15c1617e3b2b9e331e2ab7dd6f2ea"))
-	        (  45000, uint256S("0x00000000017efb152de0225848c1e2b8e03116252e0b96a56d6330467f1412d8"))
 	        (  50000, uint256S("0x0000000010f1cfdac6446f431337e3874e6b91ada402e4fe9361778f1a684572"))
-	        (  55000, uint256S("0x0000000015fc57032197d2a200fcd878e76e1c740594d7a175a89a92a9d70e95"))
-	        (  60000, uint256S("0x000000000044e8311956b7e00cda12c2fd3df21465a6411bea3a6f3de6aaf1ba"))
-	        (  65000, uint256S("0x000000000612aff5784a8b8beba682f32610c0907fdd05061ccdd006bd331fc2"))
-	        (  70000, uint256S("0x000000001a835ae450f5fc24dcfbb9a73727eb360055db2e6f004875e9e283ac"))
-	        (  75000, uint256S("0x000000000f7699c372bc333ccfe77e6e4c1dba455534f1683f87afa77f718662"))
-	        (  80000, uint256S("0x00000000382cac8c592da673d1cb3eece9747a725dc8d40420ec8c54d70353ca"))
-            (  85000, uint256S("0x00000000172eb33e1d197a00cc470dc823c64ff35ad9d28446858084fcf7acb2"))
-            (  90000, uint256S("0x0000000013a3c3b48c07adddcccf3c81dc07e47357cec312434bff9426ceb606"))
-            (  95000, uint256S("0x000000000bdf998343263166fa2d862f671c96161433630e1bc2d911e01e646e"))
             ( 100000, uint256S("0x0000000006fbc78ab84d3ddf4246d128b36c5240060cfc5ce9bb989a775bf970"))
-            ( 105000, uint256S("0x00000000083d9c4a12584c77b4a71fe4dcbd3545974971b34f83bca1e1af9d6b"))
-            ( 110000, uint256S("0x00000000123d606ceb21089d7564913ac9e1ac012dc411a5de2e26b5288d4ae6"))
-            ( 115000, uint256S("0x000000000c77a0d520021ae7e706b706d257b6ff63b8d2818269a6a0ff926f25"))
-            ( 117400, uint256S("0x000000000596de322263352224b2b9f5030fe63e003433791ecec5676755923b")),
+            ( 150000, uint256S("0x0000000005f483c970c60293a8d0508cfe6a4500359d26f84abdf302328f413d"))
+            ( 200000, uint256S("0x00000000124558e8e546ebc3a45f05952cb8476c29193c0b2d130d97450f92e4"))
+            ( 250000, uint256S("0x000000000c449edcdcd3fe2ea4a7f2910a13089065acc10a4bf7d15f00c5960e"))
+            ( 269000, uint256S("0x00000000841704dda516618c09062847e5fd6158963c59b39835ff531ab94bc0"))
         };
 
         chainTxData = ChainTxData{
-            1528882815, // * UNIX timestamp of last known number of transactions
-            162310,    // * total number of transactions between genesis and that timestamp
+            1549015815, // * UNIX timestamp of last known number of transactions
+            354527,    // * total number of transactions between genesis and that timestamp
                         //   (the tx=... number in the SetBestChain debug.log lines)
             0.1         // * estimated number of transactions per second after that timestamp
         };
