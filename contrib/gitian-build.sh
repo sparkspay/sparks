6ddbb648447dce588a6bc976d1273ba7d1490dca
Dilusha Madusanka <dilusha@gdsmc.be>
2020-10-08 12:13:31 +0530

    github url renamed

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -17,7 +17,7 @@ osx=true
 SIGNER=
 VERSION=
 commit=false
-url=https://github.com/sparkspay/sparks
+url=https://github.com/sparkspayofficial/sparks
 proc=2
 mem=2000
 lxc=true
@@ -40,7 +40,7 @@ version		Version number, commit, or branch to build. If building a commit or bra
 
 Options:
 -c|--commit	Indicate that the version argument is for a commit or branch
--u|--url	Specify the URL of the repository. Default is https://github.com/sparkspay/sparks
+-u|--url	Specify the URL of the repository. Default is https://github.com/sparkspayofficial/sparks
 -v|--verify 	Verify the gitian build
 -b|--build	Do a gitian build
 -s|--sign	Make signed binaries for Windows and Mac OSX
@@ -249,8 +249,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/sparkspay/gitian.sigs.git
-    git clone https://github.com/sparkspay/sparks-detached-sigs.git
+    git clone https://github.com/sparkspayofficial/gitian.sigs.git
+    git clone https://github.com/sparkspayofficial/sparks-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
