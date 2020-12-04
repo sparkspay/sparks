8b63e10e85a765c04b047fb10c231f4ea92c028f
Dilusha Madusanka <dilusha@gdsmc.be>
2020-12-04 14:09:40 +0530

    allow older masternodes/guardian nodes to connect during DIP3 migration

--- src/version.h
+++ src/version.h
@@ -20,10 +20,10 @@ static const int INIT_PROTO_VERSION = 209;
 static const int GETHEADERS_VERSION = 70077;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70214;
+static const int MIN_PEER_PROTO_VERSION = 70213;
 
 //! disconnect from peers which don't support Guardian nodes
-static const int GUARDIAN_PROTOCOL_VERSION = 70214;
+static const int GUARDIAN_PROTOCOL_VERSION = 70213;
 
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
