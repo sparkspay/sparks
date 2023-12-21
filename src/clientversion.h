b1885f8d354bfc75ca93da6cd346efa96d6ad2db
janaka <janaka@itplace.com>
2023-12-21 16:06:57 +0530

    Removed Dash related unnessary header files 21 dec 2023

--- src/clientversion.h
+++ src/clientversion.h
@@ -6,7 +6,7 @@
 #define BITCOIN_CLIENTVERSION_H
 
 #if defined(HAVE_CONFIG_H)
-#include <config/sparks-config.h>
+
 #include <config/bitcoin-config.h>
 #endif //HAVE_CONFIG_H
 
--- src/compat.h
+++ src/compat.h
@@ -7,7 +7,7 @@
 #define BITCOIN_COMPAT_H
 
 #if defined(HAVE_CONFIG_H)
-#include <config/sparks-config.h>
+
 #endif
 
 #include <type_traits>
